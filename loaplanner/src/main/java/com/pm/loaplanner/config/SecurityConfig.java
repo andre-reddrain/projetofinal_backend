fac4b427f911f131b86d701eae2cf5731ae23d45
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2025-12-17 23:57:00 +0000

    Configuração Spring Security. Adição de /api aos endpoints

---
+++ loaplanner/src/main/java/com/pm/loaplanner/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.pm.loaplanner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/h2/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/actuator/mappings/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2/**", "/api/**"))
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+        return http.build();
+    }
+}
