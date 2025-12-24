4a86d01fa05c8e7d594bf4d2b2ca6b3913ee017d
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2025-12-24 17:17:33 +0000

    Tabela Activities & Preenchimento da tabela.

---
+++ loaplanner/src/main/java/com/pm/loaplanner/model/Activity.java
@@ -0,0 +1,32 @@
+package com.pm.loaplanner.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "activities")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "reset_type")
+    private String resetType;
+
+    @Column(name = "max_entries")
+    private int maxEntries;
+}
