671abdaa71c7c66a0601c1546959883a47992966
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2025-12-29 16:30:44 +0000

    Autenticação (Login) com JWT. Novas dependências & tabela Users.

--- loaplanner/src/main/java/com/pm/loaplanner/config/SecurityConfig.java
+++ loaplanner/src/main/java/com/pm/loaplanner/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +14,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/h2/**").permitAll()
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/actuator/mappings/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2/**", "/api/**"))
-                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/api/**").permitAll();
+                auth.anyRequest().authenticated();
+            })
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2/**", "/api/**"))
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()));
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ loaplanner/src/main/java/com/pm/loaplanner/model/User.java
@@ -0,0 +1,32 @@
+package com.pm.loaplanner.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
