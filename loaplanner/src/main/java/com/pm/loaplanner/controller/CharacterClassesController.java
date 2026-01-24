f7814b00a733597761be68da238da157542c36b8
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-24 22:19:48 +0000

    Endpoint para recolher as CharacterClasses. Adição do icon das CharacterClasses Migration.

---
+++ loaplanner/src/main/java/com/pm/loaplanner/controller/CharacterClassesController.java
@@ -0,0 +1,26 @@
+package com.pm.loaplanner.controller;
+
+import com.pm.loaplanner.dto.CharacterClassesDTO;
+import com.pm.loaplanner.service.CharacterClassesService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/character-classes")
+public class CharacterClassesController {
+    private final CharacterClassesService characterClassesService;
+
+    public  CharacterClassesController(CharacterClassesService characterClassesService) {
+        this.characterClassesService = characterClassesService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CharacterClassesDTO>> getAllCharacterClasses() {
+        List<CharacterClassesDTO> characterClasses = characterClassesService.getAllCharacterClasses();
+        return ResponseEntity.ok().body(characterClasses);
+    }
+}
---
+++ loaplanner/src/main/java/com/pm/loaplanner/mapper/CharacterClassesMapper.java
@@ -0,0 +1,15 @@
+package com.pm.loaplanner.mapper;
+
+import com.pm.loaplanner.dto.CharacterClassesDTO;
+import com.pm.loaplanner.model.CharacterClasses;
+
+public class CharacterClassesMapper {
+    public static CharacterClassesDTO toDTO(CharacterClasses characterClasses) {
+        CharacterClassesDTO dto = new CharacterClassesDTO();
+        dto.setId(characterClasses.getId().toString());
+        dto.setType(characterClasses.getType());
+        dto.setSubtype(characterClasses.getSubtype());
+        dto.setIcon(characterClasses.getIcon());
+        return dto;
+    }
+}
